3c00129e93089cd4b178520a9d33f9e58c011040
casarin <nsgbanks@ciencias.unam.mx>
2024-02-03 22:41:52 -0600

    delete method added

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -6,15 +6,19 @@ import com.paymentchain.customer.entities.Customer;
 import com.paymentchain.customer.repository.CustomerRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -26,7 +30,7 @@ public class CustomerRestController {
     CustomerRepository repo;
 
     @GetMapping()
-    public List<Customer> findAll() {
+    public List<Customer> list() {
         return repo.findAll();
     }
 
@@ -35,5 +39,19 @@ public class CustomerRestController {
         Customer customer = repo.save(input);
         return ResponseEntity.ok(customer);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") long id) {
+        Optional<Customer> customer = repo.findById(id);
+        if (!customer.isPresent()) 
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        repo.delete(customer.get());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
     
+    @PutMapping("path/{id}")
+    public ResponseEntity<?> putMethodName(@PathVariable("id") long id, @RequestBody Customer entity) {
+        return null;
+    }
 }
