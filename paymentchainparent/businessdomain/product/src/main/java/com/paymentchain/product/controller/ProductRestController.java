a4ef3c02529278c315c0a912feddecbed0f0b1f7
casarin <nsgbanks@ciencias.unam.mx>
2024-02-04 18:35:50 -0600

    added skeletons for update and delete in product

--- paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/controller/ProductRestController.java
+++ paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/controller/ProductRestController.java
@@ -11,11 +11,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/product")
@@ -41,6 +44,16 @@ public class ProductRestController {
     public ResponseEntity<?> createProduct(@RequestBody Product input) {
         Product product = repo.save(input);
         return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable("id") long id) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable("id") long id, @RequestBody Product input) {
+        return null;
     }
     
 }
