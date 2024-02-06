e195df44bf97fb8ae0e2c358ab2aae513bb7b62e
casarin <nsgbanks@ciencias.unam.mx>
2024-02-05 22:27:53 -0600

    Added basic configuration for communication with Product to Customer

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/CustomerApplication.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/CustomerApplication.java
@@ -2,6 +2,21 @@ package com.paymentchain.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 public class CustomerApplication {
@@ -10,4 +25,30 @@ public class CustomerApplication {
 		SpringApplication.run(CustomerApplication.class, args);
 	}
 
+	private HttpClient createHttpClient() {
+		return HttpClient.create()
+            //Connection Timeout: is a period within which a connection between a client and a server must be established
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+            .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+            //Response Timeout: The maximun time we wait to receive a response after sending a request
+            .responseTimeout(Duration.ofSeconds(1))
+            // Read and Write Timeout: A read timeout occurs when no data was read within a certain 
+            //period of time, while the write timeout when a write operation cannot finish at a specific time
+            .doOnConnected(connection -> {
+                connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+            });
+	}
+
+	@Bean
+	public WebClient client() {
+		return WebClient.builder()
+		 .clientConnector(new ReactorClientHttpConnector(createHttpClient()))
+		 .baseUrl("http://localhost:8083/product")
+		 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+		 .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8083/product"))
+		 .build();
+	}
 }
